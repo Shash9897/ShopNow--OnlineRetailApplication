fa6d54bd5534afb8a569ffd19639677c22c8e31f
Shash9897 <119075307+Shash9897@users.noreply.github.com>
2024-02-10 13:27:23 +0530

    Add files via upload

---
+++ shopping.sql
@@ -0,0 +1,305 @@
+-- MySQL dump 10.13  Distrib 8.0.35, for macos13 (arm64)
+--
+-- Host: localhost    Database: SHOPPING
+-- ------------------------------------------------------
+-- Server version	8.0.35
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `ADMIN`
+--
+
+DROP TABLE IF EXISTS `ADMIN`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ADMIN` (
+  `ADMIN_ID` int NOT NULL AUTO_INCREMENT,
+  `NAME` varchar(100) NOT NULL,
+  `EMAIL` varchar(100) NOT NULL,
+  `CONTACT_NUMBER` varchar(20) DEFAULT NULL,
+  `POSITIONS` varchar(100) DEFAULT NULL,
+  `VENDOR_ID` int DEFAULT NULL,
+  PRIMARY KEY (`ADMIN_ID`),
+  KEY `VENDOR_ID` (`VENDOR_ID`),
+  CONSTRAINT `admin_ibfk_1` FOREIGN KEY (`VENDOR_ID`) REFERENCES `VENDOR` (`VENDOR_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ADMIN`
+--
+
+LOCK TABLES `ADMIN` WRITE;
+/*!40000 ALTER TABLE `ADMIN` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ADMIN` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `CART`
+--
+
+DROP TABLE IF EXISTS `CART`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `CART` (
+  `PRODUCT_ID` int DEFAULT NULL,
+  `ITEMS_AMOUNT` int DEFAULT NULL,
+  `USERNAME` varchar(50) DEFAULT NULL,
+  `BILLING_AMOUNT` decimal(10,2) DEFAULT NULL,
+  `ADDRESS` varchar(255) DEFAULT NULL,
+  KEY `PRODUCT_ID` (`PRODUCT_ID`),
+  CONSTRAINT `cart_ibfk_1` FOREIGN KEY (`PRODUCT_ID`) REFERENCES `PRODUCT` (`PRODUCT_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `CART`
+--
+
+LOCK TABLES `CART` WRITE;
+/*!40000 ALTER TABLE `CART` DISABLE KEYS */;
+/*!40000 ALTER TABLE `CART` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `CATEGORY`
+--
+
+DROP TABLE IF EXISTS `CATEGORY`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `CATEGORY` (
+  `CATEGORY_ID` int NOT NULL AUTO_INCREMENT,
+  `CATEGORY_NAME` varchar(100) DEFAULT NULL,
+  `CATEGORY_TYPE` varchar(100) DEFAULT NULL,
+  `CATEGORY_DISCOUNT` decimal(5,2) DEFAULT NULL,
+  PRIMARY KEY (`CATEGORY_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `CATEGORY`
+--
+
+LOCK TABLES `CATEGORY` WRITE;
+/*!40000 ALTER TABLE `CATEGORY` DISABLE KEYS */;
+/*!40000 ALTER TABLE `CATEGORY` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `DELIVERY_PARTNER`
+--
+
+DROP TABLE IF EXISTS `DELIVERY_PARTNER`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `DELIVERY_PARTNER` (
+  `DELIVERY_PARTNER_ID` int NOT NULL AUTO_INCREMENT,
+  `NAME` varchar(100) DEFAULT NULL,
+  `EMAIL` varchar(100) DEFAULT NULL,
+  `ADDRESS` varchar(255) DEFAULT NULL,
+  `CONTACT_NUMBER` varchar(20) DEFAULT NULL,
+  `VEHICLE_ID` int DEFAULT NULL,
+  `DELIVERY_ID` int DEFAULT NULL,
+  `STATUS` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`DELIVERY_PARTNER_ID`),
+  KEY `DELIVERY_ID` (`DELIVERY_ID`),
+  CONSTRAINT `delivery_partner_ibfk_1` FOREIGN KEY (`DELIVERY_ID`) REFERENCES `ORDER` (`ORDER_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `DELIVERY_PARTNER`
+--
+
+LOCK TABLES `DELIVERY_PARTNER` WRITE;
+/*!40000 ALTER TABLE `DELIVERY_PARTNER` DISABLE KEYS */;
+/*!40000 ALTER TABLE `DELIVERY_PARTNER` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `INVENTORY`
+--
+
+DROP TABLE IF EXISTS `INVENTORY`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `INVENTORY` (
+  `PRODUCT_ID` int NOT NULL,
+  `QUANTITY` int DEFAULT NULL,
+  `VENDOR_ID` int DEFAULT NULL,
+  `COST_PER_UNIT` decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY (`PRODUCT_ID`),
+  CONSTRAINT `inventory_ibfk_1` FOREIGN KEY (`PRODUCT_ID`) REFERENCES `PRODUCT` (`PRODUCT_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `INVENTORY`
+--
+
+LOCK TABLES `INVENTORY` WRITE;
+/*!40000 ALTER TABLE `INVENTORY` DISABLE KEYS */;
+/*!40000 ALTER TABLE `INVENTORY` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ORDER`
+--
+
+DROP TABLE IF EXISTS `ORDER`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ORDER` (
+  `ORDER_ID` int NOT NULL AUTO_INCREMENT,
+  `DELIVERY_ID` int DEFAULT NULL,
+  `ORDER_DATE` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `AMOUNT` decimal(10,2) DEFAULT NULL,
+  `PRODUCT_ID` int DEFAULT NULL,
+  `CUSTOMER_ID` int DEFAULT NULL,
+  PRIMARY KEY (`ORDER_ID`),
+  KEY `DELIVERY_ID` (`DELIVERY_ID`),
+  CONSTRAINT `order_ibfk_1` FOREIGN KEY (`DELIVERY_ID`) REFERENCES `DELIVERY_PARTNER` (`DELIVERY_PARTNER_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ORDER`
+--
+
+LOCK TABLES `ORDER` WRITE;
+/*!40000 ALTER TABLE `ORDER` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ORDER` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `PRODUCT`
+--
+
+DROP TABLE IF EXISTS `PRODUCT`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `PRODUCT` (
+  `PRODUCT_ID` int NOT NULL AUTO_INCREMENT,
+  `PRODUCT_NAME` varchar(100) DEFAULT NULL,
+  `CATEGORY_ID` int DEFAULT NULL,
+  `QUANTITY` int DEFAULT NULL,
+  `PRICE` decimal(10,2) DEFAULT NULL,
+  `DESCRIPTION` text,
+  PRIMARY KEY (`PRODUCT_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `PRODUCT`
+--
+
+LOCK TABLES `PRODUCT` WRITE;
+/*!40000 ALTER TABLE `PRODUCT` DISABLE KEYS */;
+/*!40000 ALTER TABLE `PRODUCT` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `TRANSACTIONS`
+--
+
+DROP TABLE IF EXISTS `TRANSACTIONS`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `TRANSACTIONS` (
+  `TRANSACTION_ID` int NOT NULL AUTO_INCREMENT,
+  `ORDER_ID` int DEFAULT NULL,
+  `AMOUNT` decimal(10,2) DEFAULT NULL,
+  `PAYMENT_MODE` varchar(50) DEFAULT NULL,
+  `CUSTOMER_ID` int DEFAULT NULL,
+  PRIMARY KEY (`TRANSACTION_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `TRANSACTIONS`
+--
+
+LOCK TABLES `TRANSACTIONS` WRITE;
+/*!40000 ALTER TABLE `TRANSACTIONS` DISABLE KEYS */;
+/*!40000 ALTER TABLE `TRANSACTIONS` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `USER`
+--
+
+DROP TABLE IF EXISTS `USER`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `USER` (
+  `CUSTOMER_ID` int NOT NULL AUTO_INCREMENT,
+  `NAME` varchar(100) NOT NULL,
+  `EMAIL` varchar(100) NOT NULL,
+  `ADDRESS` varchar(255) DEFAULT NULL,
+  `CONTACT_NUMBER` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`CUSTOMER_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `USER`
+--
+
+LOCK TABLES `USER` WRITE;
+/*!40000 ALTER TABLE `USER` DISABLE KEYS */;
+/*!40000 ALTER TABLE `USER` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `VENDOR`
+--
+
+DROP TABLE IF EXISTS `VENDOR`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `VENDOR` (
+  `VENDOR_ID` int NOT NULL AUTO_INCREMENT,
+  `NAME` varchar(100) NOT NULL,
+  `EMAIL` varchar(100) NOT NULL,
+  `ADDRESS` varchar(255) DEFAULT NULL,
+  `CONTACT_NUMBER` varchar(20) DEFAULT NULL,
+  `PRODUCT_ID` int DEFAULT NULL,
+  `ORDER_ID` int DEFAULT NULL,
+  PRIMARY KEY (`VENDOR_ID`),
+  KEY `PRODUCT_ID` (`PRODUCT_ID`),
+  KEY `ORDER_ID` (`ORDER_ID`),
+  CONSTRAINT `vendor_ibfk_1` FOREIGN KEY (`PRODUCT_ID`) REFERENCES `PRODUCT` (`PRODUCT_ID`),
+  CONSTRAINT `vendor_ibfk_2` FOREIGN KEY (`ORDER_ID`) REFERENCES `ORDER` (`ORDER_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `VENDOR`
+--
+
+LOCK TABLES `VENDOR` WRITE;
+/*!40000 ALTER TABLE `VENDOR` DISABLE KEYS */;
+/*!40000 ALTER TABLE `VENDOR` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-02-10 13:26:31
