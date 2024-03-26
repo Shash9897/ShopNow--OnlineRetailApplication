5efe294f4ccf2433029c29d64a053688f47a58f5
Shash9897 <119075307+Shash9897@users.noreply.github.com>
2024-03-26 12:33:10 +0530

    Delete Project directory

--- Project/DBMS/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.example.dbms {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens org.example.dbms to javafx.fxml;
-    exports org.example.dbms;
-}
--- Project/DBMS/src/main/java/org/example/dbms/MainScreenController.java
+++
@@ -1,8 +0,0 @@
-package org.example.dbms;
-import javafx.fxml.*;
-public class MainScreenController {
-
-//    public void MainScreen(){
-//
-//    }
-}
